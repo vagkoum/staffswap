fee0159ae62340c9ef1d6ea9743dff300988f15f
vagkoum <vagkoum4290@gmail.com>
2026-06-27 16:03:11 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 //    Get them from: https://app.supabase.com
 //    → Your project → Settings → API
 // ─────────────────────────────────────────────
-const SUPABASE_URL = 'https://fnqkylgekrnpvczbivzh.supabase.co/rest/v1/'
+const SUPABASE_URL = 'https://fnqkylgekrnpvczbivzh.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_zX1qS0QmH5RIkvOexSAYqQ_RqrzgYof'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
